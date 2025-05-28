51fdc2fe02bc46826178bc55d62e3936fad70205
Kerim471 <kerimsugaipov404@gmail.com>
2025-05-28 09:54:31 +0300

    На ревью№1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const cardContainer = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+function makeCard(card, deleteCard) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
+    const cardButton = cardElement.querySelector('.card__delete-button');
+    cardButton.addEventListener('click', () => {
+      deleteCard(cardElement)
+    });
 
-// @todo: Функция создания карточки
+    cardImage.src = card.link;
+    cardTitle.textContent = card.name;
 
-// @todo: Функция удаления карточки
+    return cardElement;
+};
 
-// @todo: Вывести карточки на страницу
+function deleteCard(card) {
+  card.remove();
+};
+
+initialCards.forEach(function (card) {
+  cardContainer.append(makeCard(card, deleteCard));
+});
+
